36d6c803ae4096b4f9d474d980122e5ec7691e5e
anuon <anuonn@gmail.com>
2019-07-24 20:01:54 +0200

    client server link

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send({ hi: "there" });
-// });
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("cleint/build"));
+  app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
