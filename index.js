17b579833dc69f4a86ee0f5da54ac746a0759da0
anuon <anuonn@gmail.com>
2019-07-24 19:29:22 +0200

    heroku build

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("cleint/build"));
+  const path = require("PATH");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
